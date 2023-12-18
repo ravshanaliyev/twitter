adf967c2da0a1a34bba3773162b7a669c8284f03
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-12-18 10:44:52 +0500

    error is resolved

--- app/(root)/api/auth/register/route.ts
+++ app/(root)/api/auth/register/route.ts
@@ -1,10 +1,11 @@
+import User from "@/database/user.model";
 import { connectToDatabase } from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 export async function POST(request: Request) {
     try {
         await connectToDatabase()
         const { username, email, password, name } = await request.json()
-        
+        User.create({ username, email, password, name })
     } catch (error) {
         const result = error as Error
         return NextResponse.json({ error: result.message }, { status: 400 })
