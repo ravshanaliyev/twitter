a6b6a65d6253911e2f8fab1ddc2f75bcc2ed2475
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-12-21 14:56:12 +0500

    form functionality added

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,41 @@
-import Auth from '@/components/auth'
-import React from 'react'
+"use client";
+
+import Forms from '@/components/follow-bar/forms'
+import Header from '@/components/follow-bar/header'
+import usePosts from '@/hooks/usePosts';
+import { IPost } from '@/types';
+import { Loader2 } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import React, { useState } from 'react'
 
 export default function page() {
+  const { data: session, status }: any = useSession();
+  const { data, isLoading } = usePosts();
+  const [posts, setPosts] = useState<IPost[]>([]);
   return (
-    <div className='text-3xl'>page</div>
+    <>
+      <Header label="Home" />
+      {isLoading || status === "loading" ? (
+        <div className="flex justify-center items-center h-24">
+          <Loader2 className="animate-spin text-sky-500" />
+        </div>
+      ) : (
+        <>
+          <Forms
+            placeholder="What's on your mind?"
+            setPosts={setPosts}
+            user={JSON.parse(JSON.stringify(session.currentUser))}
+          />
+          {/* {posts.map((post) => (
+            <PostItem
+              key={post._id}
+              post={post}
+              user={JSON.parse(JSON.stringify(session.currentUser))}
+              setPosts={setPosts}
+            />
+          ))} */}
+        </>
+      )}
+    </>
   )
 }
---
+++ components/follow-bar/forms.tsx
@@ -0,0 +1,95 @@
+import { IPost, IUser } from '@/types';
+import axios from 'axios';
+import React, { Dispatch, SetStateAction, useState } from 'react'
+import { toast } from '../ui/use-toast';
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
+import Button from '../ui/button';
+
+interface Props {
+    placeholder: string;
+    user: IUser;
+    setPosts: Dispatch<SetStateAction<IPost[]>>;
+    postId?: string;
+    isComment?: boolean;
+}
+
+const Forms = ({ placeholder, user, setPosts, isComment, postId }: Props) => {
+    const [body, setBody] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onSubmit = async () => {
+        try {
+            setIsLoading(true);
+            if (isComment) {
+                const { data } = await axios.post("/api/comments", {
+                    body,
+                    userId: user._id,
+                    postId,
+                });
+                const newComment = {
+                    ...data,
+                    user,
+                    likes: 0,
+                    hasLiked: false,
+                };
+                setPosts((prev) => [newComment, ...prev]);
+            } else {
+                const { data } = await axios.post("/api/posts", {
+                    body,
+                    userId: user._id,
+                });
+                const newPost = {
+                    ...data,
+                    user,
+                    likes: 0,
+                    hasLiked: false,
+                    comments: 0,
+                };
+                setPosts((prev) => [newPost, ...prev]);
+            }
+            setIsLoading(false);
+            setBody("");
+        } catch (error) {
+            setIsLoading(false);
+            toast({
+                title: "Error",
+                description: "Something went wrong. Please try again.",
+                variant: "destructive",
+            });
+        }
+    };
+
+    return (
+        <div className="border-b-[1px] border-neutral-800 px-5 py-2">
+            <div className="flex flex-row gap-4">
+                <Avatar>
+                    <AvatarImage src={user.profileImage} />
+                    <AvatarFallback>{user.name[0]}</AvatarFallback>
+                </Avatar>
+
+                <div className="w-full">
+                    <textarea
+                        className="disabled:opacity-80 peer resize-none mt-3 w-full bg-black ring-0 outline-none text-[20px] placeholder-neutral-500 text-white h-[50px]"
+                        placeholder={placeholder}
+                        disabled={isLoading}
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                        onKeyDown={(e) => e.key === "Enter" && onSubmit()}
+                    ></textarea>
+                    <hr className="opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition" />
+
+                    <div className="mt-4 flex flex-row justify-end">
+                        <Button
+                            label={isComment ? "Reply" : "Post"}
+                            classNames="px-8"
+                            disabled={isLoading || !body}
+                            onClick={onSubmit}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Forms;
