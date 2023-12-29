a0a08ea3f00c7abaed6c258d2edd397d6d7c5589
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-12-29 11:11:43 +0500

    user followers

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,11 +2,14 @@
 
 import User from "@/database/user.model"
 import { connectToDatabase } from "../mongoose"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../auth-options"
 
 export async function getUserById(userId: string) {
     try {
         await connectToDatabase()
         const user = await User.findById(userId)
+        const { currentuser }: any = await getServerSession(authOptions);
         const filteredUser = {
             _id: user._id,
             name: user.name,
@@ -19,6 +22,7 @@ export async function getUserById(userId: string) {
             createdAt: user.createdAt,
             followers: user.followers?.length || 0,
             following: user.following?.length || 0,
+            isFollowing: user.followers?.includes(currentuser?._id) || false
         }
         return filteredUser
     } catch (error) {
